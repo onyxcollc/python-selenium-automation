1fc74d05325c1dfe7999acb405147c81ba0c2498
Nicholas O <onyxcollc@gmail.com>
2025-06-09 21:02:01 -0700

    Target Website Updates

--- features/steps/target_search_steps.py
+++ features/steps/target_search_steps.py
@@ -55,4 +55,4 @@ def click_sign_in_button(context):
 def verify_sign_in(context):
     element = context.driver.find_element(By.CSS_SELECTOR,".styles_ndsHeading__HcGpD.styles_fontSize1__i0fbt")
     actual_text = element.text
-    assert actual_text == 'Sign in or create account'
+    assert 'Sign in or create account'in actual_text, f"Error, expected 'Sign in or create account' in actual text: {actual_text}"
